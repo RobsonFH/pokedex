8be8ffa3c9829f482bf68744420d30ac6a11a7bc
unknown <robson_herculano@hotmail.com>
2023-09-21 09:40:56 -0300

    fase3

--- pokedex/src/data.js
+++ pokedex/src/data.js
@@ -19,8 +19,64 @@ const pokemonData = [
         id: 3,
         nome: "Bulbasaur",
         imagem: "bulbasaur.jpg",
-        tipo: "grama",
+        tipo: "Grama",
         descricao: "Bulbassauro pode ser visto cochilando sob luz solar intensa. Há uma semente na sua parte traseira. Por absorvendo os raios do sol, a semente cresce progressivamente maior",
+      },
+
+      {
+        id: 4,
+        nome: "Gyarados",
+        imagem: "gyarados.jpg",
+        tipo: " Água/Voador",
+        descricao: "Gyarados é um Pokémon serpente aquática que, quando enfurecido, se transforma em uma criatura feroz com presas afiadas e poderosos ataques aquáticos. É temido por sua fúria.",
+      },
+
+      {
+        id: 5,
+        nome: "Snorlax",
+        imagem: "snorlax.jpg",
+        tipo: "Normal",
+        descricao: " Snorlax é um Pokémon gigante e preguiçoso que passa a maior parte do tempo dormindo. Ele é conhecido por bloquear caminhos inteiros com seu corpo massivo quando está cochilando",
+      },
+
+      {
+        id: 6,
+        nome: "Jigglypuff",
+        imagem: "jigglypuff.jpg",
+        tipo: "Normal/Fada",
+        descricao: "Jigglypuff é um Pokémon fada que tem uma habilidade especial chamada Canto, que faz com que qualquer um que ouça sua música adormeça instantaneamente. Ele desenha rostos engraçados nos rostos dos adormecidos.",
+      },
+
+      {
+        id: 7,
+        nome: "Machamp",
+        imagem: "machamp.jpg",
+        tipo: "Lutador",
+        descricao: "Machamp é um Pokémon musculoso e poderoso que possui quatro braços. Ele é um mestre nas artes marciais e é conhecido por sua força física extraordinária.",
+      },
+
+      {
+        id: 8,
+        nome: "Gengar",
+        imagem: "gengar.jpg",
+        tipo: "Fantasma/Venenoso",
+        descricao: "Gengar é um Pokémon do tipo Fantasma/Venenoso que gosta de assustar as pessoas à noite. Ele é capaz de se tornar invisível e atravessar paredes",
+      },
+
+      {
+        id: 9,
+        nome: "Dragonite",
+        imagem: "dragonite.jpg",
+        tipo: "Dragão/Voador",
+        descricao: "Dragonite é um Pokémon majestoso que voa pelos céus e é conhecido por sua natureza gentil e protetora. Ele é um dragão amigável e poderoso",
+      },
+
+      {
+        id: 10,
+        nome: "Lucario",
+        imagem: "lucario.jpg",
+        tipo: "Lutador/Aço",
+        descricao: "Lucario é um Pokémon do tipo Lutador/Aço que é conhecido por sua habilidade de sentir as emoções das pessoas e dos outros Pokémon. Ele usa essa habilidade para tomar decisões sábias em batalha.",
       },
   ];
   
